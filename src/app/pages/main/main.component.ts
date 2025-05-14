cac6902cfcbf991b7a386e844f778817007e7b2d
ChrlyDev <cf9220543@gmail.com>
2025-05-13 20:37:04 -0500

    Se cambio el estilo del main y el header, y se arreglaron rutas

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-main',
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
